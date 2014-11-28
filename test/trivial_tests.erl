97e88818a88f18d4f41b3e1cf10cfd31346a586f
Viktor <viktor@textalk.se>
2014-11-28 19:22:56 +0100

    Add more tests

---
+++ test/trivial_tests.erl
@@ -0,0 +1,32 @@
+%% @doc Tests trivial things that we don't test in other tests, mostly for coverage.
+-module(trivial_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+-include_lib("amqp_client/include/amqp_client.hrl").
+
+gen_servers_test_() ->
+    {setup,
+     fun () -> error_logger:tty(false) end,
+     fun (_) -> error_logger:tty(true) end,
+     fun () ->
+         %% We silence the error log for these assertions. They talk a lot.
+         ?assertEqual({reply, ok, x}, queuemailerl_amqp_mgr:handle_call(foo, {self(), tag}, x)),
+         ?assertEqual({noreply, x}, queuemailerl_amqp_mgr:handle_cast(foo, x)),
+         ?assertEqual({noreply, x}, queuemailerl_amqp_mgr:handle_info(foo, x)),
+         ?assertEqual({ok, x}, queuemailerl_amqp_mgr:code_change(42, x, foo)),
+
+         ?assertError(badarg, queuemailerl_listener:handle_call(foo, {self(), tag}, x)),
+         ?assertEqual({noreply, x}, queuemailerl_listener:handle_info(foo, x)),
+         ?assertEqual({noreply, x},
+                      queuemailerl_listener:handle_info({#'basic.deliver'{},
+                                                         #amqp_msg{payload = <<"{}">>}},
+                                                        x)),
+         ?assertEqual({ok, x}, queuemailerl_listener:code_change(42, x, foo)),
+
+         ?assertError(badarg, queuemailerl_smtp_worker:handle_call(foo, {self(), tag}, x)),
+         ?assertError(badarg, queuemailerl_smtp_worker:handle_cast(foo, x)),
+         ?assertEqual({noreply, x}, queuemailerl_smtp_worker:handle_info(foo, x)),
+         ?assertEqual({ok, x}, queuemailerl_smtp_worker:code_change(42, x, foo)),
+
+         ok
+     end}.
