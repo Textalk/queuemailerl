cd8bb42c94523fd217061eeda5eb02e5b21630a3
Emil Falk <emph@emph.se>
2014-12-08 11:26:48 +0100

    Made the rabbitmq download script play nice when queuemailerl is a dependency.

--- download-rabbit.sh
+++ download-rabbit.sh
@@ -8,10 +8,14 @@
 VERSION=3.3.5
 BASE_URL=http://www.rabbitmq.com/releases/rabbitmq-erlang-client
 for LIB in rabbit_common amqp_client ; do
-    if [ ! -e deps/$LIB ]; then
-        FILE=$LIB-$VERSION.ez
-        URL=$BASE_URL/v$VERSION/$FILE
-        wget -O $FILE -nc $URL && unzip $FILE && mv $LIB-$VERSION deps/$LIB
-        rm $FILE
+
+    ARCHIVE_DIR=$REBAR_DEPS_DIR/$LIB-$VERSION
+    ARCHIVE=$ARCHIVE_DIR.ez
+    LIB_DIR=$REBAR_DEPS_DIR/$LIB
+
+    if [ ! -e $LIB_DIR ]; then
+        URL=$BASE_URL/v$VERSION/$(basename $ARCHIVE)
+        wget -O $ARCHIVE -nc $URL && unzip $ARCHIVE -d $REBAR_DEPS_DIR && mv $ARCHIVE_DIR $LIB_DIR
+        rm $ARCHIVE
     fi
 done
